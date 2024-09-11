c9131ec49271f0742c0093fba16a9d48f931b317
it22092274 <128246276+it22092274@users.noreply.github.com>
2024-09-12 00:07:53 +0530

    finalize to now

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { conn } from "./config/db";
 import express from "express";
 import cors from  'cors'
 import dotenv from 'dotenv'
+import { Request, Response } from "express";
 
 dotenv.config()
 
@@ -10,6 +11,10 @@ app.use(express.json())
 app.use(cors())
 
 conn()
+
+app.get('/', async (req: Request, res: Response) => {
+    res.send('hello auth v')
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server running');    
